ba768c174604639971acaa2bddd30e8f17754eae
Lakshya Soni <103238896+lakshya5500@users.noreply.github.com>
2022-06-24 10:23:59 +0530

    Delete e-comRestApi directory

--- e-comRestApi/src/main/java/com/ecom/api/repository/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ecom.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecom.api.entity.Category;
-
-public interface CategoryRepo extends JpaRepository<Category, String>{
-
-}
